5eedb969830cf74173733fbe5de312e7899e566c
kunal <kunalgk1999@gmail.com>
2021-06-07 22:25:27 +0530

    Updates

---
+++ precache-manifest.835ff9d7943d45b2c299aa5c3e446e0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6190f1431a5a235ea9537478b91e379",
+    "url": "./index.html"
+  },
+  {
+    "revision": "86e1b4bf97c92d7fe7db",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "a65e4e7053ccd2e73e85",
+    "url": "./static/js/2.22d3258c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.22d3258c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e1b4bf97c92d7fe7db",
+    "url": "./static/js/main.04b119d3.chunk.js"
+  },
+  {
+    "revision": "6722f99d2d0febe62f9d",
+    "url": "./static/js/runtime-main.1a965084.js"
+  }
+]);
