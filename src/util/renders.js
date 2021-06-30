b8485bfb5eca8636221581e73ddb11f68451cb2b
Kunal Kamble <kunalgk1999@gmail.com>
2021-06-30 08:55:56 +0530

    ✅ Added test for `util` (#21)
    COVERING_TEST: :heavy_check_mark: 
    FIXES: #20

--- src/util/renders.js
+++ src/util/renders.js
@@ -2,5 +2,5 @@ import { useRef } from 'react'
 
 export const useRenderCount = (key) => {
   const renderCount = useRef(0)
-  console.log('Redered', key.label, renderCount.current++)
+  console.log('Rendered', key.label, renderCount.current++)
 }
