1ff669954db693815cb652077080ba415653dce6
kunal <kunalgk1999@gmail.com>
2021-07-19 08:47:52 +0530

    Added support to size property in form - If no size is passed for will have medium size.
    COVERING_TEST: :heavy_check_mark:
    ISSUE: #29

--- src/DynamicForm/__tests__/StringForm.test.js
+++ src/DynamicForm/__tests__/StringForm.test.js
@@ -3,6 +3,7 @@
 import { toBeInTheDocument } from '@testing-library/jest-dom'
 import { fireEvent, render } from '@testing-library/react'
 import React from 'react'
+import { DynamicFormSize } from '../../Schema'
 import { ROLE_INPUT_STRING, ROLE_LABEL_TEXTY } from '../roles'
 import { StringForm } from '../TextyForm'
 
@@ -11,11 +12,12 @@ const GET_SIMPLE_STRING_FORM = ({
   defaultValue = 'Default Value',
   placeholder = 'String',
   key = null,
+  size = DynamicFormSize.medium,
   onChange = () => {}
 }) => {
   return (
     <StringForm
-      schema={{ label, defaultValue, placeholder }}
+      schema={{ label, defaultValue, placeholder, size }}
       atKey={key}
       onChange={onChange}
     />
@@ -71,4 +73,44 @@ test('StringForm with empty label will not have any label', () => {
   const component = render(GET_SIMPLE_STRING_FORM({ label: '' }))
 
   expect(component.queryByRole(ROLE_LABEL_TEXTY)).toBeNull()
+})
+
+describe('StringForm size is', () => {
+  test('small if small schema size is passed', () => {
+    const component = render(
+      GET_SIMPLE_STRING_FORM({ size: DynamicFormSize.small })
+    )
+
+    const input = component.getByRole(ROLE_INPUT_STRING)
+    expect(input.classList).toContain('form-control-sm')
+    expect(component.container.firstChild.classList).toContain('input-group-sm')
+  })
+
+  test('medium if medium schema size is passed', () => {
+    const component = render(
+      GET_SIMPLE_STRING_FORM({ size: DynamicFormSize.medium })
+    )
+
+    const input = component.getByRole(ROLE_INPUT_STRING)
+    expect(input.classList).toContain('form-control-md')
+    expect(component.container.firstChild.classList).toContain('input-group-md')
+  })
+
+  test('medium if no schema size is passed', () => {
+    const component = render(GET_SIMPLE_STRING_FORM({ size: undefined }))
+
+    const input = component.getByRole(ROLE_INPUT_STRING)
+    expect(input.classList).toContain('form-control-md')
+    expect(component.container.firstChild.classList).toContain('input-group-md')
+  })
+
+  test('large if large schema size is passed', () => {
+    const component = render(
+      GET_SIMPLE_STRING_FORM({ size: DynamicFormSize.large })
+    )
+
+    const input = component.getByRole(ROLE_INPUT_STRING)
+    expect(input.classList).toContain('form-control-lg')
+    expect(component.container.firstChild.classList).toContain('input-group-lg')
+  })
 })
