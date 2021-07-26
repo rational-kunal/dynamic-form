e2f4c28afb37b3b6e1e0ff0aae8917e708349dc1
Kunal Kamble <kunalgk1999@protonmail.com>
2021-07-26 08:37:56 +0530

    Revert "✨ Add support for Sizeable button (#36)"
    This reverts commit d843d7df3ea6e3c1b8e784f62d0119f19ba592a2.

--- src/DynamicForm/__tests__/DynamicForm.test.js
+++ src/DynamicForm/__tests__/DynamicForm.test.js
@@ -1,25 +1,9 @@
 /* eslint-disable no-undef */
 import { fireEvent, render } from '@testing-library/react'
 import React from 'react'
-import { DynamicFormSize, DynamicFormType } from '../../Schema'
+import { DynamicFormType } from '../../Schema'
 import { DynamicForm } from '../index'
 import { ROLE_INPUT_DYNAMIC_SUBMIT, ROLE_INPUT_STRING } from '../roles'
-
-const GET_SIMPLE_DYNAMIC_FORM = ({
-  schema = {},
-  onChange = undefined,
-  onSubmit = undefined,
-  submitSize = undefined
-}) => {
-  return (
-    <DynamicForm
-      schema={schema}
-      onChange={onChange}
-      onSubmit={onSubmit}
-      submitSize={submitSize}
-    />
-  )
-}
 
 test('DynamicForm with correct props matches snapshot', () => {
   const component = render(
@@ -88,51 +72,4 @@ test('DynamicForm will call onSubmit correctly if submit is tapped', (done) => {
 
   const submitInput = component.getByRole(ROLE_INPUT_DYNAMIC_SUBMIT)
   fireEvent.click(submitInput)
-})
-
-describe('DynamicForm size is', () => {
-  test('small if small submit button size is passed', () => {
-    const component = render(
-      GET_SIMPLE_DYNAMIC_FORM({
-        onSubmit: () => {},
-        submitSize: DynamicFormSize.small
-      })
-    )
-
-    const input = component.getByRole(ROLE_INPUT_DYNAMIC_SUBMIT)
-    expect(input.classList).toContain('btn-sm')
-  })
-
-  test('medium if medium submit button size is passed', () => {
-    const component = render(
-      GET_SIMPLE_DYNAMIC_FORM({
-        onSubmit: () => {},
-        submitSize: DynamicFormSize.medium
-      })
-    )
-
-    const input = component.getByRole(ROLE_INPUT_DYNAMIC_SUBMIT)
-    expect(input.classList).toContain('btn-md')
-  })
-
-  test('medium if no submit button size is passed', () => {
-    const component = render(
-      GET_SIMPLE_DYNAMIC_FORM({ onSubmit: () => {}, submitSize: undefined })
-    )
-
-    const input = component.getByRole(ROLE_INPUT_DYNAMIC_SUBMIT)
-    expect(input.classList).toContain('btn-md')
-  })
-
-  test('large if large submit button size is passed', () => {
-    const component = render(
-      GET_SIMPLE_DYNAMIC_FORM({
-        onSubmit: () => {},
-        submitSize: DynamicFormSize.large
-      })
-    )
-
-    const input = component.getByRole(ROLE_INPUT_DYNAMIC_SUBMIT)
-    expect(input.classList).toContain('btn-lg')
-  })
 })
